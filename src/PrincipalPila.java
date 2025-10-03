5524855dac0bf307fec745c328546feebe799863
odell1 <pablo.lledo@gmail.com>
2025-10-03 12:54:58 +0200

    d

--- src/PrincipalPila.java
+++ src/PrincipalPila.java
@@ -2,8 +2,20 @@ public class PrincipalPila {
 
     public static void main(String[] args) {
         
-        
+        MiPilila<Character> pila=new MiPilila<>();
 
+        pila.push('X');
+        pila.push('F');
+        pila.push('O');
+
+        pila.imprimirElementos();
+
+        pila.pop();
+        pila.pop();
+        pila.pop();
+
+        //Debe dar error
+        pila.pop();
 
     }//main
     
