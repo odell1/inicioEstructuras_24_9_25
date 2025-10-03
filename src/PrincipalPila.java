43c5a672bf2ffcd21321c3ebfabbb469ad03ec2d
odell1 <pablo.lledo@gmail.com>
2025-10-03 13:27:31 +0200

    ss

--- src/PrincipalPila.java
+++ src/PrincipalPila.java
@@ -17,6 +17,28 @@ public class PrincipalPila {
         //Debe dar error
         pila.pop();
 
+
+
+        //pila.imprimirElementos();
+        String expresion="((3+4)*5)+7+2)"; 
+        boolean pasamos=false;
+        for (int i = 0; i < expresion.length(); i++) {
+            char mi=expresion.charAt(i);
+            if(mi=='(') pila.push(mi);
+
+            if(mi==')') {
+                if (pila.esVacia()) pasamos=true; 
+                    else
+                     pila.pop();
+            }
+
+
+        }//for
+
+        if (pila.esVacia() && !pasamos) System.out.println("Es correcta la expresión");
+        else  System.out.println("La cagaste BurlanCaster.");
+
+
     }//main
     
 }//PrincipalPila
